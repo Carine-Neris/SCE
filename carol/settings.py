c916b681ee7b5153f812cc28ab641fadbb0f908b
Carine Neris <carine_neris@hotmail.com>
2024-04-10 17:58:40 -0300

    adicionado variavel para uso do banco de prod

--- carol/settings.py
+++ carol/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from decouple import config
+from dj_database_url import parse as dburl 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -65,14 +66,17 @@ WSGI_APPLICATION = 'carol.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+            'default': config('DATABASE', cast=dburl),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
